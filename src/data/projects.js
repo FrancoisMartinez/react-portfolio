9a122f4f291eaabdeab30dbde5378c79cebde558
FrancoisMartinez <160101566+FrancoisMartinez@users.noreply.github.com>
2024-09-15 16:56:25 -0400

    some changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,34 @@
 const projects = [
+
     {
         id: 1,
-        titleKey: 'projects.projectOne.title',
-        descriptionKey: 'projects.projectOne.description',
-
+        titleKey: 'projects.paperTrading.title',
+        descriptionKey: 'projects.paperTrading.description',
+        // Optional additional fields
+        year: '2024',
+        type: 'Personal',
+        // Add more fields like 'image', 'link', etc., if available
     },
     {
         id: 2,
-        titleKey: 'projects.projectTwo.title',
-        descriptionKey: 'projects.projectTwo.description',
-
+        titleKey: 'projects.petAdoption.title',
+        descriptionKey: 'projects.petAdoption.description',
+        year: '2024',
+        type: 'Academic',
+    },
+    {
+        id: 3,
+        titleKey: 'projects.marketForecaster.title',
+        descriptionKey: 'projects.marketForecaster.description',
+        year: '2024',
+        type: 'Personal',
+    },
+    {
+        id: 4,
+        titleKey: 'projects.othelloGame.title',
+        descriptionKey: 'projects.othelloGame.description',
+        year: '2024',
+        type: 'Academic',
     },
 ];
 
