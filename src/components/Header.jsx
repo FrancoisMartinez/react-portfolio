0f1da57ceadd481501b4abca70edf90b8ea9c94e
FrancoisMartinez <160101566+FrancoisMartinez@users.noreply.github.com>
2024-09-15 21:45:29 -0400

    some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect, useRef } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
 import styles from '../styles/Header.module.css';
 import { ThemeContext } from '../contexts/ThemeContext';
@@ -9,7 +9,22 @@ const Header = () => {
     const { theme, toggleTheme } = useContext(ThemeContext);
     const { t, i18n } = useTranslation();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const navRef = useRef(null);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 768 && isMenuOpen) {
+                setIsMenuOpen(false); // Close the menu if resizing above 768px
+            }
+        };
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Clean up event listener on unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [isMenuOpen]);
 
     // Language toggle function
     const changeLanguage = (lng) => {
@@ -21,52 +36,68 @@ const Header = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
-    // Close menu when clicking outside
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (navRef.current && !navRef.current.contains(event.target) && isMenuOpen) {
-                setIsMenuOpen(false);
-            }
-        };
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [isMenuOpen]);
-
     return (
         <header className={styles.header}>
-            <a href="/" className={styles.logo}>
-                FrançoisMartinez
-            </a>
-            <button
-                className={styles.menuToggle}
-                onClick={toggleMenu}
-                aria-label={isMenuOpen ? 'Close Menu' : 'Open Menu'}
-                aria-controls="primary-navigation"
-                aria-expanded={isMenuOpen}
-            >
-                <FaBars />
-            </button>
-            <nav
-                id="primary-navigation"
-                className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}
-                ref={navRef}
-            >
-                <Link to="about" smooth={true} duration={500} onClick={() => setIsMenuOpen(false)}>
+            <div className={styles.headerContainer}>
+                <a href="/" className={styles.logo}>
+                    FrançoisMartinez
+                </a>
+                <button
+                    className={styles.menuToggle}
+                    onClick={toggleMenu}
+                    aria-label={isMenuOpen ? 'Close Menu' : 'Open Menu'}
+                    aria-controls="primary-navigation"
+                    aria-expanded={isMenuOpen}
+                >
+                    <FaBars/>
+                </button>
+            </div>
+
+            <nav className={styles.nav}>
+                <Link to="about" smooth={true} duration={500}>
                     {t('header.about')}
                 </Link>
-                <Link to="projects" smooth={true} duration={500} onClick={() => setIsMenuOpen(false)}>
+                <Link to="projects" smooth={true} duration={500}>
                     {t('header.projects')}
                 </Link>
-                <Link to="contact" smooth={true} duration={500} onClick={() => setIsMenuOpen(false)}>
+                <Link to="contact" smooth={true} duration={500}>
+                    {t('header.contact')}
+                </Link>
+                <a
+                    href={i18n.language === 'en' ? "./data/Martinez_F_40300308_CV_En.pdf" : "./data/Martinez_F_40300308_CV_Fr.pdf"}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {t('header.downloadCV')}
+                </a>
+                <button onClick={toggleTheme} className={styles.themeToggle}>
+                    {theme === 'light' ? '🌙' : '☀️'}
+                </button>
+                <button
+                    onClick={() => {
+                        const newLanguage = i18n.language === 'en' ? 'fr' : 'en';
+                        changeLanguage(newLanguage);
+                    }}
+                    className={styles.languageSwitcher}
+                >
+                    {i18n.language === 'en' ? 'Fr' : 'En'}
+                </button>
+            </nav>
+            <nav className={`${styles.navMobile} ${isMenuOpen ? styles.navOpen : ''}`}>
+                <Link to="about" smooth={true} duration={500} onClick={toggleMenu}>
+                    {t('header.about')}
+                </Link>
+                <Link to="projects" smooth={true} duration={500} onClick={toggleMenu}>
+                    {t('header.projects')}
+                </Link>
+                <Link to="contact" smooth={true} duration={500} onClick={toggleMenu}>
                     {t('header.contact')}
                 </Link>
                 <a
                     href="/cv.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
-                    onClick={() => setIsMenuOpen(false)}
+                    onClick={toggleMenu}
                 >
                     {t('header.downloadCV')}
                 </a>
