2d1c1122616d005a99fa021077cf3b09042fc083
FrancoisMartinez <160101566+FrancoisMartinez@users.noreply.github.com>
2025-05-02 18:09:30 -0400

    new projects

--- public/data/projects.js
+++ public/data/projects.js
@@ -4,37 +4,36 @@ const projects = [
         id: 1,
         titleKey: 'projects.paperTrading.title',
         descriptionKey: 'projects.paperTrading.description',
-        // Optional additional fields
-        year: '2024',
-        type: 'Personal',
-        // Add more fields like 'image', 'link', etc., if available
     },
     {
         id: 2,
         titleKey: 'projects.petAdoption.title',
         descriptionKey: 'projects.petAdoption.description',
-        year: '2024',
-        type: 'Academic',
     },
     {
         id: 3,
         titleKey: 'projects.marketForecaster.title',
         descriptionKey: 'projects.marketForecaster.description',
-        year: '2024',
-        type: 'Personal',
     },
     {
         id: 4,
         titleKey: 'projects.othelloGame.title',
         descriptionKey: 'projects.othelloGame.description',
-        year: '2024',
-        type: 'Academic',
     },
     {
         id: 5,
+        titleKey: 'projects.peerReview.title',
+        descriptionKey: 'projects.peerReview.description',
     },
     {
         id: 6,
+        titleKey: 'projects.fishingAssistant.title',
+        descriptionKey: 'projects.fishingAssistant.description',
+    },
+    {
+        id: 7,
+        titleKey: 'projects.casperAI.title',
+        descriptionKey: 'projects.casperAI.description',
     }
 ];
 
