33d2e701eaa40908379ae8336079fa2e006d1c91
FrancoisMartinez <160101566+FrancoisMartinez@users.noreply.github.com>
2025-05-07 17:46:22 -0400

    fix event on mobile

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../styles/Event.module.css';
 import Card from './Card.jsx';
 
-const Event = ({ children, date, side, dateWidth }) => {
+const Event = ({ children, date, side, dateWidth, eventWidth }) => {
     
     const timeline = (
         <div className={styles.timeline}>
@@ -16,7 +16,7 @@ const Event = ({ children, date, side, dateWidth }) => {
         <>
             {side === 'left' && 
                 <div className={styles.event}>
-                    <Card style={{ width: '24rem', margin: '1rem 0' }}>{children}</Card>
+                    <Card style={{ width: eventWidth, margin: '1rem 0' }}>{children}</Card>
                     {timeline}
                     <div className={styles.date} style={{ textAlign: 'start', width: dateWidth}}><p>{date}</p></div>
                 </div>
@@ -25,7 +25,7 @@ const Event = ({ children, date, side, dateWidth }) => {
                 <div className={styles.event}>
                     <div className={styles.date} style={{ textAlign: 'end', width: dateWidth }}><p>{date}</p></div>
                     {timeline}
-                    <Card style={{ width: '24rem', margin: '1rem 0' }}>{children}</Card>
+                    <Card style={{ width: eventWidth, margin: '1rem 0' }}>{children}</Card>
                 </div>
             }
         </>
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -7,13 +7,14 @@ import timelineEvents from '/public/data/timelineEvents.js';
 const Timeline = () => {
     const [side, setSide] = useState(2);
     const [dateWidth, setDateWidth] = useState('24rem');
-    const [endPosition, setEndPosition] = useState('50%');
+    const [eventWidth, setEventWidth] = useState('24rem');
     const { t } = useTranslation();
     
     useEffect(() => {
         const handleResize = () => {
             setSide(window.innerWidth < 850 ? 0 : 2);
-            setDateWidth(window.innerWidth < 850 ? '12rem' : '24rem');
+            setDateWidth(window.innerWidth < 850 ? '6rem' : '24rem');
+            setEventWidth(window.innerWidth < 850 ? '12rem' : '24rem');
         };
     
         handleResize();
@@ -25,15 +26,15 @@ const Timeline = () => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.topEnd} style={{ marginLeft: dateWidth }}></div>
+            <div className={styles.topEnd} style={{ marginLeft: dateWidth, marginRight: eventWidth }}></div>
             
                 {timelineEvents.map((timelineEvent) => (
-                    <Event key={timelineEvent.id} style={{ width: '18.75rem' }} side={timelineEvent.id % side == 0 ? 'left' : 'right'} date={t(timelineEvent.dateKey)} dateWidth={dateWidth}>
+                    <Event key={timelineEvent.id} style={{ width: '18.75rem' }} side={timelineEvent.id % side == 0 ? 'left' : 'right'} date={t(timelineEvent.dateKey)} dateWidth={dateWidth} eventWidth={eventWidth}>
                             <h2>{t(timelineEvent.titleKey)}</h2>
                             <p>{t(timelineEvent.descriptionKey)}</p>
                     </Event>
                 ))}
-            <div className={styles.bottomEnd} style={{ marginLeft: dateWidth }}></div>
+            <div className={styles.bottomEnd} style={{ marginLeft: dateWidth, marginRight: eventWidth }}></div>
         </div>
     );
 }
